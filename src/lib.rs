31aae17aeeda2d9e9056c177c3994eeff8ffc67e
cfr2ak <cfr2ak@gmail.com>
2020-01-07 22:01:29 +0900

    Implement default method for Qubit

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
 mod willard {
+    struct Qubit{
+    }
+
+    impl Default for Qubit {
+	fn default -> Qubit {
+	    Qubit{state: [1.0, 0.0], phase: 0.0}
+	}
+    }
+
     #[cfg(test)]
     mod tests {
+	use super::*;
+	
 	#[test]
 	fn test_qubit() {
 	    let qubit = Qubit::default();
