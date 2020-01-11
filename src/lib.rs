c1421f6fbbe06456734411474af6e0804d994b1f
cfr2ak <cfr2ak@gmail.com>
2020-01-11 18:36:38 +0900

    Change module structure

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@ fn measure(qubit: &mut Qubit) -> u32 {
 }
 
 pub mod gate {
-    use crate::willard::Qubit;
+    use crate::Qubit;
     use num::complex::Complex;
 
     pub fn not(qubit: &mut Qubit) {
@@ -61,6 +61,7 @@ pub mod gate {
     #[cfg(test)]
     mod tests {
 	use super::*;
+	use num::complex::Complex;
 
 	#[test]
 	fn test_normalize_phase() {
@@ -69,16 +70,51 @@ pub mod gate {
 	    let want = (1.0, Complex::new(0.0, 1.0));
 	    assert_eq!(got, want);
 	}
+
+	#[test]
+	fn test_not() {
+	    let mut qubit = Qubit::default();
+
+	    not(&mut qubit);
+
+	    assert_eq!(qubit.state, (0.0, Complex{re: 1.0, im: 0.0}));
+	}
+
+	#[test]
+	fn test_h() {
+	    // Test on a qubit of state [1.0, 0.0]
+	    let mut qubit0 = Qubit::default();
+
+	    h(&mut qubit0);
+
+	    let root_two = (2.0 as f32).sqrt();
+
+	    let got = qubit0.state;
+	    let want = (1.0 / root_two, Complex{re: 1.0 / root_two, im: 0.0});
+
+	    assert_eq!(got, want);
+
+	    // Test on a qubit of state [0.0, 1.0]
+	    let mut qubit1 = Qubit::default();
+	    not(&mut qubit1);
+
+	    h(&mut qubit1);
+
+	    let got = qubit1.state;
+	    let want = (1.0 / root_two, Complex{re: -1.0 / root_two, im: 0.0});
+
+	    assert_eq!(got, want);
+	}
     }
 }
 
 pub mod alg {
-    use crate::willard;
+    use crate::*;
 
     pub fn qrn() -> u32 {
-	let mut qubit = willard::Qubit::default();
-	willard::gate::h(&mut qubit);
-	return willard::measure(&mut qubit);
+	let mut qubit = Qubit::default();
+	gate::h(&mut qubit);
+	return measure(&mut qubit);
     }
 }
 
@@ -94,43 +130,12 @@ mod tests {
 	assert_eq!(qubit.state, (1.0, Complex{re: 0.0, im: 0.0}));
     }
 
-    #[test]
-    fn test_not() {
-	let mut qubit = Qubit::default();
-
-	gate::not(&mut qubit);
-
-	assert_eq!(qubit.state, (0.0, Complex{re: 1.0, im: 0.0}));
-    }
-
-    #[test]
-    fn test_h() {
-	// Test on a qubit of state [1.0, 0.0]
-	let mut qubit0 = Qubit::default();
-
-	gate::h(&mut qubit0);
-
-	let root_two = (2.0 as f32).sqrt();
-
-	let got = qubit0.state;
-	let want = (1.0 / root_two, Complex{re: 1.0 / root_two, im: 0.0});
-
-	assert_eq!(got, want);
-
-	// Test on a qubit of state [0.0, 1.0]
-	let mut qubit1 = Qubit::default();
-	gate::not(&mut qubit1);
-
-	gate::h(&mut qubit1);
-
-	let got = qubit1.state;
-	let want = (1.0 / root_two, Complex{re: -1.0 / root_two, im: 0.0});
-
-	assert_eq!(got, want);
-    }
+    
 
     #[test]
     fn test_collapes_of_state() {
+	use crate::gate;
+
 	let mut qubit = Qubit::default();
 
 	gate::h(&mut qubit);
