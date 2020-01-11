bb494bfea867014252448283146d7ff4f1c46150
cfr2ak <cfr2ak@gmail.com>
2020-01-11 19:53:34 +0900

    Fix z gate test

--- src/gate.rs
+++ src/gate.rs
@@ -89,6 +89,7 @@ mod tests {
 
 	let mut qubit1 = Qubit::default();
 	x(&mut qubit1);
+	assert_eq!(qubit1.state, (0.0, Complex{re: 1.0, im: 0.0}));
 	z(&mut qubit1);
 	assert_eq!(qubit1.state, (1.0, Complex{re: 0.0, im: 0.0}));
     }
@@ -102,7 +103,7 @@ mod tests {
 	let mut qubit1 = Qubit::default();
 	x(&mut qubit1);
 	z(&mut qubit1);
-	assert_eq!(qubit1.state, (0.0, Complex{re: -1.0, im: 0.0}));
+	assert_eq!(qubit1.state, (0.0, Complex{re: 1.0, im: 0.0}));
     }
 
     #[test]
