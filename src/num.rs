0af8c9808fb097d269d8e3b41a2cef416455ce10
cfr2ak <cfr2ak@gmail.com>
2020-02-01 19:59:26 +0900

    Add test for the measurement

--- src/num.rs
+++ src/num.rs
@@ -34,12 +34,44 @@ mod tests {
     use super::*;
     use num::complex::Complex;
 
+
     #[test]
-    fn test_init_qubyte() {
+    fn test_h_gate() {
+	let qubyte = Qubyte::default();
+
+	qubyte.h(0);
     }
 
     #[test]
-    fn test_apply_gate_to_qubit_in_qubyte() {
+    fn test_collapse_of_state() {
+	// Prepare the qubyte and the qubit
+	let qubyte = Qubyte::default();
+	qubyte.h(0);
+	let want = qubyte.measure(0);
+	for _n in 0..100 {
+	    let got = qubyte.maasure(0);
+	    assert_eq!(got, want);
+	}
+    }
+
+    #[test]
+    fn test_x_gate() {
+    }
+
+    #[test]
+    fn test_y_gate() {
+    }
+
+    #[test]
+    fn test_z_gate() {
+    }
+
+    #[test]
+    fn test_swap_gate() {
+    }
+
+    #[test]
+    fn test_cnot_gate() {
     }
 }
     
